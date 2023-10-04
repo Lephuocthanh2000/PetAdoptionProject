995b006adfb860d2eba5005c8f5650fb4cfab101
lephuocthanh2000 <82993963+Lephuocthanh2000@users.noreply.github.com>
2023-10-04 13:05:11 +0700

    update

--- contracts/PetAdoption.sol
+++ contracts/PetAdoption.sol
@@ -1,10 +1,43 @@
 //SPDX-License-Identifier: UNLICENSE
 pragma solidity ^0.8.21;
+import "hardhat/console.sol";
 
 contract PetAdoption {
     address public owner;
+    uint public indexPet = 0;
+    uint[] public allAdoptedPets;
 
-    constructor() {
+    mapping(uint => address) public petIdToOwnerAddress;
+    mapping(address => uint[]) public owneAddressToPetList;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor(uint initialPetIndex) {
         owner = msg.sender;
+        indexPet = initialPetIndex;
+    }
+
+    function getOwner() public view returns (address) {
+        return owner;
+    }
+
+    function addPet() external onlyOwner returns (bool) {
+        indexPet++;
+        return true;
+    }
+
+    function adoptPet(uint adoptIdx) public {
+        require(adoptIdx < indexPet, "Pet index out of bound");
+        require(
+            petIdToOwnerAddress[adoptIdx] == address(0),
+            "Pet has already adopted"
+        );
+        console.log("Adopting pet: ", adoptIdx);
+        console.log("new Owner: ", petIdToOwnerAddress[adoptIdx]);
+        owneAddressToPetList[msg.sender].push(adoptIdx);
+        allAdoptedPets.push(adoptIdx);
     }
 }
--- test/PetAdoption.js
+++ test/PetAdoption.js
@@ -1,9 +1,65 @@
-const { expect } = require('chai ')
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
+const {
+  Contract,
+} = require('hardhat/internal/hardhat-network/stack-traces/model')
 
 describe('Pet Adoption', () => {
+  let contractAddress, _contract
+  let ownerAddress
+  let accountOther
+  const petCount = 10
+  async function deployContract() {
+    const [owner, account] = await ethers.getSigners()
+    const PetAdoption = await ethers.getContractFactory('PetAdoption')
+    const contract = await PetAdoption.deploy(petCount)
+    return { owner, contract, account }
+  }
+  before(async () => {
+    const { owner, contract, account } = await deployContract()
+    contractAddress = await contract.getOwner()
+    ownerAddress = owner.address
+    accountOther = account.address
+    _contract = contract
+  })
   describe('Deployment', () => {
-    it('Should set the right owner ', () => {
-      expect(true).to.equal(true)
+    it('Should set the right owner ', async function () {
+      expect(await contractAddress).to.equal(ownerAddress)
+      console.log('Contract Owner: ' + contractAddress)
+      console.log('Owner: ' + ownerAddress)
+    })
+    it('Should get the right owner ', async function () {
+      expect(await contractAddress).to.equal(ownerAddress)
+      console.log('Contract Owner: ' + contractAddress)
+      console.log('Owner: ' + ownerAddress)
+    })
+  })
+  describe('Add Pet', () => {
+    it('Should revert with right error in case of orther account', () => {
+      expect(_contract.connect(accountOther).addPet()).to.be.revertedWith(
+        'Only a owner can add a new pet'
+      )
+    })
+    it('Should increace pet index', async function () {
+      await _contract.addPet()
+      expect(await _contract.indexPet()).to.equal(petCount + 1)
+    })
+  })
+  describe('Adopt Pet', () => {
+    it('Should revert with right error in case of value out of bounds', () => {
+      expect(_contract.adoptPet(petCount)).to.be.revertedWith(
+        'Pet index out of bound'
+      )
+      expect(_contract.adoptPet(-1)).to.be.revertedWith('value out of bounds')
+    })
+    it('Should revert with right error in case of Pet has already adopted', async () => {
+      await _contract.adoptPet(petCount - 1)
+      expect(
+        _contract.connect(accountOther).adoptPet(petCount - 1)
+      ).to.be.revertedWith('Pet has already adopted')
+    })
+    it('Adopt Pet successfull ', async () => {
+      await expect(_contract.adoptPet(1)).not.to.be.reverted
     })
   })
 })
