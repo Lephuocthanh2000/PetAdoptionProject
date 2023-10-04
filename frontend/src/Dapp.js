d4889f887c344a4637ac690389f9baa8074177ea
lephuocthanh2000 <82993963+Lephuocthanh2000@users.noreply.github.com>
2023-10-04 21:37:37 +0700

    update wallet connect

--- frontend/src/Dapp.js
+++ frontend/src/Dapp.js
@@ -2,11 +2,12 @@ import { Navbar } from './components/navbar'
 import { PetItem } from './components/petItem'
 import { TxError } from './components/txError'
 import { WalletNotDetected } from './components/WalletNotDetected'
+import { WalletConnect } from './components/WalletConnect'
 import { useEffect } from 'react'
 import { useState } from 'react'
 export function Dapp() {
   const [pets, setPets] = useState([])
-
+  const [selectedAddress, setSelectedAddress] = useState(undefined)
   useEffect(() => {
     async function fetchPets() {
       const res = await fetch('/pets.json')
@@ -18,6 +19,9 @@ export function Dapp() {
   }, [])
   if (window.ethereum === undefined) {
     return <WalletNotDetected />
+  }
+  if (selectedAddress === undefined) {
+    return <WalletConnect />
   }
   return (
     <div className="container">
---
+++ frontend/src/components/WalletConnect.js
@@ -0,0 +1,11 @@
+export function WalletConnect() {
+  return (
+    <div className="container">
+      <div>Please, connect to wallet to enter the application.</div>
+      <br />
+      <div>
+        <button className="action-button">Connect to Wallet</button>
+      </div>
+    </div>
+  )
+}
