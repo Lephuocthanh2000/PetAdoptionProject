d849d247b4055d97f3b7434ac6c6edb556e60758
lephuocthanh2000 <82993963+Lephuocthanh2000@users.noreply.github.com>
2023-10-04 16:20:00 +0700

    update frontend

---
+++ frontend/src/Dapp.js
@@ -0,0 +1,33 @@
+import { Navbar } from './components/navbar'
+import { PetItem } from './components/petItem'
+import { TxError } from './components/txError'
+import { useEffect } from 'react'
+import { useState } from 'react'
+export function Dapp() {
+  const [pets, setPets] = useState([])
+
+  useEffect(() => {
+    async function fetchPets() {
+      const res = await fetch('/pets.json')
+      const data = await res.json()
+      setPets(data)
+    }
+
+    fetchPets()
+  }, [])
+  return (
+    <div className="container">
+      <TxError />
+      <br />
+      <div className="navbar-container">
+        <Navbar />
+      </div>
+      {/* {JSON.stringify(pets)} */}
+      <div className="items">
+        {pets.map((pet) => (
+          <PetItem key={pet.id} pet={pet} />
+        ))}
+      </div>
+    </div>
+  )
+}
